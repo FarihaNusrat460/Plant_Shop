061c31de1a7793ab24474fd3c34694b8f2ff95a7
BrishtyBilash <67582453+BrishtyBilash@users.noreply.github.com>
2021-08-25 22:32:48 +0600

    Blog_model_create

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, default="")
+    bloggers_name = models.CharField(max_length=100, default="")
+    blog = models.TextField(max_length=5000, default="")
+
+    image = models.ImageField(upload_to='images/blogs/',blank=True)
+
+
+    def __str__(self):
+        return self.title
+
