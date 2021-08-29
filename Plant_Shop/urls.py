5c0bdec0dff3bdc3f4695c686ea4cae391f7e256
FarihaNusrat460 <fariha17nusrat@gmail.com>
2021-08-29 17:52:21 +0600

    registration

--- Plant_Shop/urls.py
+++ Plant_Shop/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from User import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Registration/',user_views.registration)
 ]
---
+++ User/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+def registration(request):
+    user_form = UserCreationForm()
+    if request.method == "POST":
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, 'Account is created successfully')
+
+    context = {
+        'form': user_form,
+    }
+
+    return render(request, 'User/registration.html', context)
