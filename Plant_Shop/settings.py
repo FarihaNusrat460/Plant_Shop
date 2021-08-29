38cacfb92ff888d0641eea677256a4295537f50d
Ruhin11 <18101011@uap-bd.edu>
2021-08-29 13:28:55 +0600

    .

--- Plant_Shop/settings.py
+++ Plant_Shop/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Equipment.apps.EquipmentConfig',
     'Blog.apps.BlogConfig',
-    'Plant.apps.PlantConfig'
+    'Plant.apps.PlantConfig',
+
+
 ]
 
 MIDDLEWARE = [
