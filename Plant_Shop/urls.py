2038391724e552c2d75326e2a02e5ca58b446245
Ruhin11 <18101011@uap-bd.edu>
2021-08-30 20:37:14 +0600

    login logout

--- Plant_Shop/urls.py
+++ Plant_Shop/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from User import views as user_views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Registration/',user_views.registration)
+    path('Registration/',user_views.registration),
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
